8d2409a1ed0c7b95e7739a27fa09d05855d440bf
zlieberman <zlieberman99@gmail.com>
2022-07-20 19:23:09 -0400

    getting heroku deploy to work

--- ReadingTool/ReadingTool/settings.py
+++ ReadingTool/ReadingTool/settings.py
@@ -126,7 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('assets', os.path.join(PROJECT_DIR, '../static')),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
