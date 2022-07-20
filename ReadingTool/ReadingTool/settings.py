e833ca2c80c8da4574ad9da9f857e5b335d47d9e
zlieberman <zlieberman99@gmail.com>
2022-07-20 19:19:50 -0400

    getting heroku deploy to work

--- ReadingTool/ReadingTool/settings.py
+++ ReadingTool/ReadingTool/settings.py
@@ -128,36 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Configure Django App for Heroku.
-
-import django_on_heroku
-import dotenv
-import dj_database_url
-
-django_on_heroku.settings(locals())
-
-BASE_DIR = Path(__file__).resolve().root
-
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
-# Add these at the very last line of settings.py
-options = DATABASES['default'].get('OPTIONS', {})
-options.pop('sslmode', None)
