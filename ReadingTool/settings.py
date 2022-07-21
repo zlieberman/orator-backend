96da68f4b755e30493006d81cff81508e865b48d
zlieberman <zlieberman99@gmail.com>
2022-07-20 20:44:26 -0400

    updating settings to get heroku deployment workinggit

--- ReadingTool/settings.py
+++ ReadingTool/settings.py
@@ -144,3 +144,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
