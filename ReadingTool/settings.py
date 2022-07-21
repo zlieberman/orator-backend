b5cb470eed1323c7c15f952b52a5092bcd03b1d1
zlieberman <zlieberman99@gmail.com>
2022-07-21 14:41:05 -0400

    changed database from sqllite to postgres for heroku deployment

--- ReadingTool/settings.py
+++ ReadingTool/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'ReadingTool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'orator',
+        'USER': 'postgres',
+        'PASSWORD': 'oratorpassword99',
+        'PORT': '5432',
+        'HOST': 'localhost'
     }
 }
 
