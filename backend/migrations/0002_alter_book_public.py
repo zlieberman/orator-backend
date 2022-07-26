e4ec7cab73118d0f80879fefc4680e3b746c2914
zlieberman <zlieberman99@gmail.com>
2022-07-26 16:33:53 -0400

    updated migrations

--- backend/migrations/0002_alter_book_public.py
+++ backend/migrations/0002_alter_book_public.py
@@ -15,4 +15,9 @@ class Migration(migrations.Migration):
             name='public',
             field=models.BooleanField(default=False, null=True),
         ),
+         migrations.AlterField(
+            model_name='Assignment',
+            name='correctness_level',
+            fields=models.CharField(choices=[('NONE', 'None'), ('CLOSE', 'Close'), ('EXACT', 'Exact')], default='EXACT', max_length=5),
+        ),
     ]
